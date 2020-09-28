2035f771bf65f0b531619bb7dc9e2b114e4016be
Enoch Lee <enoch.ymlee@gmail.com>
2020-09-28 20:01:59 +0800

    0928

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		log.info("request");
 		return "Greetings from Spring Boot!";
 	}
 
